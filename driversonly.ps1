fbaa13a426a2afcbf629f2cdc816563305ab0cfd
Kevin Friedberg <kfriedberg@users.noreply.github.com>
2022-09-12 14:00:58 -0400

    Create driversonly.ps1

---
+++ driversonly.ps1
@@ -0,0 +1,40 @@
+$UpdateSvc = New-Object -ComObject Microsoft.Update.ServiceManager            
+$UpdateSvc.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"")  
+
+$Session = New-Object -ComObject Microsoft.Update.Session           
+$Searcher = $Session.CreateUpdateSearcher() 
+
+$Searcher.ServiceID = '7971f918-a847-4430-9279-4a52d1efe18d'
+$Searcher.SearchScope =  1 # MachineOnly
+$Searcher.ServerSelection = 3 # Third Party
+
+$Criteria = "IsInstalled=0 and Type='Driver'"
+Write-Host('Searching Driver-Updates...') -Fore Green  
+$SearchResult = $Searcher.Search($Criteria)          
+$Updates = $SearchResult.Updates
+
+#Show available Drivers...
+$Updates | select Title, DriverModel, DriverVerDate, Driverclass, DriverManufacturer | fl
+
+$UpdatesToDownload = New-Object -Com Microsoft.Update.UpdateColl
+$updates | % { $UpdatesToDownload.Add($_) | out-null }
+Write-Host('Downloading Drivers...')  -Fore Green  
+$UpdateSession = New-Object -Com Microsoft.Update.Session
+$Downloader = $UpdateSession.CreateUpdateDownloader()
+$Downloader.Updates = $UpdatesToDownload
+$Downloader.Download()
+
+$UpdatesToInstall = New-Object -Com Microsoft.Update.UpdateColl
+$updates | % { if($_.IsDownloaded) { $UpdatesToInstall.Add($_) | out-null } }
+
+Write-Host('Installing Drivers...')  -Fore Green  
+$Installer = $UpdateSession.CreateUpdateInstaller()
+$Installer.Updates = $UpdatesToInstall
+$InstallationResult = $Installer.Install()
+if($InstallationResult.RebootRequired) {
+	Write-Host('Reboot required! please reboot now..') -Fore Red
+	$tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment
+	$tsenv.Value('WUDriverReboot') = 'True'
+} else {
+	Write-Host('Done..') -Fore Green
+}
